c3f8b7aec97cd114e0b0e75b85bb7cbfaa6355ac
Assed Mustapha Kacemi <assed@Asseds-MacBook-Pro.local>
2026-01-18 01:53:03 +0100

    feat(algorithm_medium/utils.c) making fucntion for indexation making steps into algorithms chunks

--- algorithms/algorithme_medium/algorithme_medium1.c
+++ algorithms/algorithme_medium/algorithme_medium1.c
@@ -6,7 +6,7 @@
 /*   By: mkacemi <mkacemi@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 01:22:56 by mkacemi           #+#    #+#             */
-/*   Updated: 2026/01/18 01:30:49 by mkacemi          ###   ########.fr       */
+/*   Updated: 2026/01/18 01:51:11 by mkacemi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,63 @@ int *stack_to_array(t_stack *a)
         arr[i++] = tmp->value;
         tmp = tmp->next;
     }
-    return (NULL);
+    return (arr);
+}
+
+void    sort_array(int *arr, int size)
+{
+    int i;
+    int j;
+    int tmp;
+
+    i = 0;
+    while (i < size - 1)
+    {
+        j = 0;
+        while (j < size - i - 1)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+            j++;
+        }
+        i++;
+    }
+}
+
+int get_index(int *arr, int size, int value)
+{
+    int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (arr[i] == value)
+            return (i);
+        i++;
+    }
+    return (-1);
+}
+
+void    index_stack(t_stack *a)
+{
+    int     *arr;
+    t_node  *tmp;
+
+    arr = stack_to_array(a);
+    if (!arr)
+        return ;
+    sort_array(arr, a->size);
+    tmp = a->top;
+    while (tmp)
+    {
+        tmp->value = get_index(arr, a->size, tmp->value);
+        tmp = tmp->next;
+        if (tmp == a->top)
+            break ;
+    }
+    free(arr);
 }
